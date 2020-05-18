0250f59f126534d3fab73a6b9a208afe3e9454c0
Nazmul Kabir <nazmul.kabir1216@gmail.com>
2020-05-18 00:59:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   min_value=500
   min_key=nil
-  name_hash.each do 
+  name_hash.each do |key, value| 
     p "#{key}: #{value}"
     if min_value>value
       min_value = value
