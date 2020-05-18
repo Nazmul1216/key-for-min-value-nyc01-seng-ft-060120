911aed38da21c6b8b5caa561d9cf881bc0d0f046
Nazmul Kabir <nazmul.kabir1216@gmail.com>
2020-05-18 00:50:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_value=500
-  
-   if min_value>element
-     min_value=element
-   end 
+ 
 name_hash.each do 
 p "#{key}: #{value}"
 min_value=500
