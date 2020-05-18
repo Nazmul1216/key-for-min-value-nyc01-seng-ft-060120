95eabf85213f90bebd2aefcefe5f66c4243a67c7
Nazmul Kabir <nazmul.kabir1216@gmail.com>
2020-05-18 01:02:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,11 @@ def key_for_min_value(name_hash)
   min_value=500
   min_key=nil
   name_hash.each do |key, value| 
-    if min_value>value
-      min_value =value
+    if min_value > value
+      min_value = value
       min_key = key
     end
-    p min_key
+    
   end
+  p min_key
 end
